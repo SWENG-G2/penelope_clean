27d4ffe939e69de64e35b4d3532f5fff209541f7
Giuseppe Barillari <joe2k01dev@gmail.com>
2023-01-23 14:46:20 +0000

    Initial commit
    	* Import from https://start.spring.io/#!type=gradle-project&language=java&platformVersion=2.7.5&packaging=jar&jvmVersion=19&groupId=sweng&artifactId=penelope&name=penelope&description=Demo%20project%20for%20Spring%20Boot&packageName=sweng.penelope&dependencies=web,data-jpa,postgresql
    
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

---
+++ src/main/java/sweng/penelope/PenelopeApplication.java
@@ -0,0 +1,13 @@
+package sweng.penelope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenelopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenelopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sweng/penelope/PenelopeApplicationTests.java
@@ -0,0 +1,13 @@
+package sweng.penelope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenelopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
