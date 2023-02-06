96980f269c4722283b12bd82fba3b8926e75d3f0
Roman Kulev <rk1126@york.ac.uk>
2023-02-06 00:14:02 +0000

    Testing can't create if no access to campus; can't create if parameters missing; can remove bird

--- src/test/java/sweng/penelope/controllers/BirdControllerTest.java
+++ src/test/java/sweng/penelope/controllers/BirdControllerTest.java
@@ -1,6 +1,8 @@
 package sweng.penelope.controllers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.security.KeyPair;
@@ -9,6 +11,7 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.util.Locale;
 
+import org.apache.catalina.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,16 +21,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 
 import sweng.penelope.auth.RSAUtils;
 import sweng.penelope.entities.ApiKey;
+import sweng.penelope.entities.Bird;
 import sweng.penelope.entities.Campus;
 import sweng.penelope.repositories.ApiKeyRepository;
+import sweng.penelope.repositories.BirdRepository;
 import sweng.penelope.repositories.CampusRepository;
 import sweng.penelope.services.StorageService;
 
@@ -51,6 +58,9 @@ public class BirdControllerTest {
 
     @Autowired
     private CampusRepository campusRepository;
+
+    @Autowired
+    private BirdRepository birdRepository;
 
     @MockBean
     private StorageService storageService;
@@ -79,17 +89,17 @@ public class BirdControllerTest {
         Campus campus = new Campus();
         campus.setName("test campus");
         campus.setAuthor(IDENTITY);
-        campus.setId(5345L);
-        testCampusId = campus.getId();
-
         campusRepository.save(campus);
+        
+        testCampusId = campus.getId();
     }
-
+    
     @AfterEach
     public void cleanUp() {
         // Clean all
         apiKeyRepository.deleteAll();
         campusRepository.deleteAll();
+        birdRepository.deleteAll();
     }
 
     @Test
@@ -100,6 +110,7 @@ public class BirdControllerTest {
         // Mock loading key
         Mockito.doReturn(mockAdminPrivateKey.getEncoded()).when(storageService).loadKey(IDENTITY);
 
+        // Bird parameters
         MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
         parameters.add("name", "A");
         parameters.add("listImageURL", "A");
@@ -121,13 +132,80 @@ public class BirdControllerTest {
     }
 
     @Test
-    public void canCreateIfCampusIsThere() throws Exception {
+    public void cannotCreateIfNoAccessToCampus() throws Exception {
         // Get admin auth key
-        String key = AuthUtils.getKeyForIdentity(mockAdminPublicKey, IDENTITY, 5345);
+        String key = AuthUtils.getKeyForIdentity(mockAdminPublicKey, IDENTITY, Math.toIntExact(testCampusId));
 
         // Mock loading key
         Mockito.doReturn(mockAdminPrivateKey.getEncoded()).when(storageService).loadKey(IDENTITY);
 
+        // Insert fake user
+        ApiKey apiKey = new ApiKey();
+        apiKey.setAdmin(false);
+        apiKey.setIdentity(USER_IDENTITY);
+        apiKey.setOwnerName("testing user");
+        apiKeyRepository.save(apiKey);
+
+        // Bird parameters
+        MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
+        parameters.add("name", "A");
+        parameters.add("listImageURL", "A");
+        parameters.add("heroImageURL", "A");
+        parameters.add("soundURL", "A");
+        parameters.add("aboutMe", "A");
+        parameters.add("aboutMeVideoURL", "A");
+        parameters.add("location", "A");
+        parameters.add("locationImageURL", "A");
+        parameters.add("diet", "A");
+        parameters.add("dietImageURL", "A");
+
+        // Identity is not privileged; Credentials mismatch.
+        MockHttpServletRequestBuilder request = post(formatAddress("new", Long.toString(testCampusId, 10))).header("IDENTITY", USER_IDENTITY)
+                .header("KEY", key).params(parameters);
+
+        mockMvc.perform(request)
+                .andExpect(status().isForbidden());
+
+    }
+
+    @Test
+    public void cannotCreateIfParametersMissing() throws Exception {
+        // Get admin auth key
+        String key = AuthUtils.getKeyForIdentity(mockAdminPublicKey, IDENTITY, Math.toIntExact(testCampusId));
+
+        // Mock loading key
+        Mockito.doReturn(mockAdminPrivateKey.getEncoded()).when(storageService).loadKey(IDENTITY);
+
+        // Bird parameters
+        MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
+        parameters.add("name", null);
+        parameters.add("listImageURL", "");
+        parameters.add("heroImageURL", "");
+        parameters.add("soundURL", "");
+        parameters.add("aboutMe", "");
+        parameters.add("aboutMeVideoURL", "");
+        parameters.add("location", "");
+        parameters.add("locationImageURL", "");
+        parameters.add("diet", "");
+        parameters.add("dietImageURL", "");
+
+        MockHttpServletRequestBuilder request = post(formatAddress("new", Long.toString(testCampusId, 10))).header("IDENTITY", IDENTITY)
+                .header("KEY", key).params(parameters);
+
+        mockMvc.perform(request)
+                .andExpect(status().is4xxClientError());
+
+    }
+
+    @Test
+    public void canCreateIfEverythingValid() throws Exception {
+        // Get admin auth key
+        String key = AuthUtils.getKeyForIdentity(mockAdminPublicKey, IDENTITY, Math.toIntExact(testCampusId));
+
+        // Mock loading key
+        Mockito.doReturn(mockAdminPrivateKey.getEncoded()).when(storageService).loadKey(IDENTITY);
+
+        // Bird parameters
         MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
         parameters.add("name", "A");
         parameters.add("listImageURL", "A");
@@ -148,6 +226,44 @@ public class BirdControllerTest {
 
     }
 
+    @Test
+    public void canRemoveBirdWithCredentials() throws Exception{
+        // Mock loading key
+        Mockito.doReturn(mockAdminPrivateKey.getEncoded()).when(storageService).loadKey(IDENTITY);
 
+        // Mock deleting key
+        Mockito.doReturn(true).when(storageService).removeKey(IDENTITY);
+
+        // Insert fake campus
+        Campus temp_campus = new Campus();
+        temp_campus.setName("test campus");
+        temp_campus.setId(1L);
+        temp_campus.setAuthor(IDENTITY);
+        campusRepository.save(temp_campus);
+
+        testCampusId = temp_campus.getId();
+
+        // Insert fake bird
+        Bird bird = new Bird();
+        bird.setId(1L);
+        bird.setAuthor(IDENTITY);
+        bird.setCampus(temp_campus);
+        // BROKEN
+        birdRepository.save(bird);
+
+        // Get admin auth key
+        String key = AuthUtils.getKeyForIdentity(mockAdminPublicKey, IDENTITY, Math.toIntExact(testCampusId));
+
+        // Bird parameters
+        MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
+        parameters.add("id", Long.toString(bird.getId(), 10));
+
+        MockHttpServletRequestBuilder request = delete(formatAddress("remove", Long.toString(testCampusId, 10))).header("IDENTITY", IDENTITY)
+                .header("KEY", key).params(parameters);
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk());
+
+    }
 
 }
