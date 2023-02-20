b4648b07ff889058cde32aea5a48874f7a089598
Roman Kulev <rk1126@york.ac.uk>
2023-02-20 07:23:16 +0000

    Testing hero slide is created correctly

---
+++ src/test/java/sweng/penelope/xml/BirdXMLTest.java
@@ -0,0 +1,67 @@
+package sweng.penelope.xml;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.nio.charset.StandardCharsets;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.junit.jupiter.api.Test;
+
+class BirdXMLTest {
+    private static final String PRESENTATION_TITLE = "The test presentation.";
+    private static final String PRESENTATION_AUTHOR = "Calypso";
+    private static final String TEST_CAMPUS_NAME = "FortKnox";
+    private static final String TEST_IMAGE_URL = "ChadBird.png";
+    private static final String TEST_AUDIO_URL = "Bruh.mp3";
+    private static final String TEST_ABOUT_ME = "I love gym";
+    private static final String TEST_ABOUT_ME_VIDEO_URL = "AboutMeVideoLink.url";
+    private static final String TEST_DIET = "I love protein";
+    private static final String TEST_DIET_IMAGE_URL = "DietImageLink.url";
+    private static final String TEST_LOCATION =  "Basement";
+    private static final String TEST_LOCATION_IMAGE_URL = "MyLocationLink.url";
+    private static final Long ITEM_ID = 69L;
+
+    private static final XMLConfiguration xmlConfiguration = new XMLConfiguration(PRESENTATION_AUTHOR, PRESENTATION_TITLE, ITEM_ID);
+
+    private BirdXML slideDuckXML() {
+        BirdXML birdXML = null;
+        try {
+            birdXML = new BirdXML(xmlConfiguration);
+            birdXML.addHeroSlide("audioURL", "imageURL");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return birdXML;
+    }
+
+    @Test
+    void canCreateXML() {
+        BirdXML birdXML = slideDuckXML();
+
+        assertNotEquals(null, birdXML);
+    }
+
+    @Test
+    public void heroSlideIsCorrect() throws Exception {
+        BirdXML birdXML = new BirdXML(xmlConfiguration);
+        birdXML.addHeroSlide(TEST_AUDIO_URL, TEST_IMAGE_URL);
+
+        byte[] test_byte = birdXML.getBytes();
+        String xmlStr = new String(test_byte, StandardCharsets.UTF_8);
+        Document document = DocumentHelper.parseText(xmlStr);
+
+        Element presentation = document.getRootElement();
+        Element slide = presentation.element("slide");
+        Element image = slide.element("image");
+        Element audio = slide.element("audio");
+
+        if (slide == null || image == null || audio == null) {
+            fail("Parameters are null");
+        }
+    }
+}
