0898796e6ebef2ed6962dca47d2ca16af4a2716a
Giuseppe Barillari <joe2k01dev@gmail.com>
2023-03-13 11:41:33 +0000

    Enable caching
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- src/main/java/sweng/penelope/PenelopeApplication.java
+++ src/main/java/sweng/penelope/PenelopeApplication.java
@@ -2,8 +2,10 @@ package sweng.penelope;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PenelopeApplication {
 
 	public static void main(String[] args) {
